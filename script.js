1da7d9245bdb4eae6ba8b36f920aa4d28dd2472b
nicotav <nicolastaveras01@gmail.com>
2022-12-09 13:48:45 -0500

    function generateTarget -> generate a random number from 0 - 9
    function compareGuesses -> takes three parameters (humanGuess),
    (ComputerGuess) y el (secretTarget).
    
    then it determine the absolute value of the human and computer guess.
    
    then it returns false if absolute value of the (humanGuess) is
     greater than (computerGuess), returns true in every other scenario.
    
    function updateScore -> takes one string as a parameter (winner) and
    it update the players score adding one value to the player who wom.
    
    function advanceRound -> add a value to the (currentRoungNumber).

--- script.js
+++ script.js
@@ -2,5 +2,30 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+function generateTarget(){
+    return Math.floor(Math.random()*10,2);
+}
 
+
+function compareGuesses(human, computer, secretTarget){
+    let humanGuess = Math.abs(human-secretTarget);
+    let computerGuess = Math.abs(computer-secretTarget);
+
+    if(humanGuess>computerGuess){
+        return false;
+    }
+
+    return true;
+}
+
+function updateScore(winner){
+    if(winner ==='human'){
+        humanScore=humanScore+1;
+        return;
+    }
+    computerScore=computerScore+1;
+}
+
+function advanceRound(){
+    currentRoundNumber = currentRoundNumber+1;
+}
